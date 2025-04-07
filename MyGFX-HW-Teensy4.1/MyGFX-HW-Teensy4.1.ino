2ff64130f85bbea3233b42de173b127f64bfd69c
Brandon Van Pelt <49537228+BrandonVP@users.noreply.github.com>
2025-04-06 22:07:54 -0500

    init setup

---
+++ MyGFX-HW-Teensy4.1/MyGFX-HW-Teensy4.1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MyGFX_HW_Teensy4.ino
+ Created:	4/6/2025 9:42:35 PM
+ Author:	bvanpelt
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
